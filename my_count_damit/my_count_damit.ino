695306a418fce344156de53c8776b770c742bdbb
shiva <venkatasivareddyiot@gmail.com>
2020-01-29 15:04:19 +0530

    my second file

---
+++ my_count_damit/my_count_damit.ino
@@ -0,0 +1,32 @@
+int led=13;
+int button=2;
+int count=0;
+int ledState=HIGH;
+int buttoncurrent;
+int buttonPrevious=LOW;
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(button,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+ buttoncurrent=digitalRead(button);
+   if(buttoncurrent==HIGH && buttonPrevious==LOW)
+   {
+    count=count+1;
+    if(ledState==HIGH)
+    {
+    ledState=LOW;
+   }
+   else
+   {
+    ledState=HIGH;
+   }
+   }
+   digitalWrite(led,ledState);
+   buttonPrevious=buttoncurrent;
+   Serial.println(count);
+   delay(1000);
+}
